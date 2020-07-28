69147273d5d0c5afa27704b82381e07a78592b8d
Jose Rios <lithium@hotmail.es>
2020-07-28 09:58:31 -0400

    Creacion de proyecto Spring, solo con las dependencias basicas

---
+++ src/test/java/cl/jrios/ProyectoIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.jrios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
